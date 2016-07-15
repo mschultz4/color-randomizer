8b4358201e95d8538a80f5d3a01eae965cb64839
Matt Schultz <mschultz4@gmail.com>
2016-07-14 21:25:41 -0400

    added colors file

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-module.exports = function(){
- return '#'+Math.floor(Math.random()*16777215).toString(16);
-};
+var colors = require('./colors.js');
+
+function random(options) {
+    var color;
+    if (options.format === 'hex') {
+        color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    } else if (options.format === 'name') {
+        color = colors[Math.floor(Math.random() * colors.length)]['name'];
+    } else if (options.format === 'rgb') {
+        color = [
+            Math.floor(Math.random() * 256),
+            Math.floor(Math.random() * 256),
+            Math.floor(Math.random() * 256)
+        ];
+    }
+
+    return color;
+}
+
+module.exports = random;
